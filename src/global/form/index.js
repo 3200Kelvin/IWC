4ed64e13cf2c1a9b8d8efa903ddba7eadd6000c0
Sergey Vorobyev <syvorobyev@gmail.com>
2025-06-25 16:42:03 +0200

    deploy

--- src/global/form/index.js
+++ src/global/form/index.js
@@ -1,6 +1,7 @@
 import './style.scss';
 
 const STUDENT_VALUE = 'Medical Student';
+const OTHER_STAGE_VALUE = 'Other';
 const FOUND_THROUGH_OTHER_VALUE = 'Other';
 
 export const useForm = () => {
@@ -10,6 +11,8 @@ export const useForm = () => {
     }
 
     const stageSelect = form.querySelector('select[name="career-stage"]');
+    const otherCareerStageInput = form.querySelector('input[name="career stage other"]');
+    const otherCareerStageGroup = graduationDateInput.closest('.form__question');
     const graduationDateInput = form.querySelector('input[name="graduation date"]');
     const graduationDateGroup = graduationDateInput.closest('.form__question');
     const foundThroughSelect = form.querySelector('select[name="found-us-through"]');
@@ -34,7 +37,22 @@ export const useForm = () => {
     };
 
     function handleStageChange() {
-        if (stageSelect.value === STUDENT_VALUE) {
+        toggleGraduationDate(stageSelect.value === STUDENT_VALUE);
+        toggleOtherCareerStage(stageSelect.value === OTHER_STAGE_VALUE);
+    }
+
+    function toggleOtherCareerStage(isShown = false) {
+        if (isShown) {
+            otherCareerStageInput.required = true;
+            otherCareerStageGroup.style.display = 'block';
+        } else {
+            otherCareerStageInput.required = false;
+            otherCareerStageGroup.style.display = 'none';
+        }
+    }
+
+    function toggleGraduationDate(isShown = false) {
+        if (isShown) {
             graduationDateInput.required = true;
             graduationDateGroup.style.display = 'block';
         } else {
