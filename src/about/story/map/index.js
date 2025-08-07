82acd4f606fbe8764f881dfd9ec589b1b9aa0ed3
Sergey Vorobyev <syvorobyev@gmail.com>
2025-08-07 12:37:56 +0200

    deploy

--- src/about/story/map/index.js
+++ src/about/story/map/index.js
@@ -69,7 +69,7 @@ export const useStoryMap = (block) => {
         label: container.querySelector('.story__map__state p'),
     };
 
-    const DELAY = 600;
+    const DELAY = 500;
     let current = -1;
     let timeout;
     const allElementsToShow = [];
